bdf56d6489c07d569adcdf4edf07a4409d7e4485
Lavinia Maria Răduță <lavinia.maria.raduta07@gmail.com>
2022-01-12 01:20:20 +0200

    Added some comments for city sort

--- src/sorting/SortByCity.java
+++ src/sorting/SortByCity.java
@@ -4,7 +4,6 @@ import entities.Child;
 import enums.Cities;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class SortByCity implements SortStrategy {
     @Override
@@ -12,6 +11,9 @@ public class SortByCity implements SortStrategy {
         Map<Cities, Integer> cityWithCount = new HashMap<>();
         Map<Cities, LinkedList<Double>> cityWithScores = new HashMap<>();
         Map<Cities, Double> cityWithAverage = new HashMap<>();
+        // simultaneously create a map with the key the city and
+        // the value the number of children and one with city and a list of the
+        // average scores of the children from that city
         for (Child child : children) {
             if (cityWithCount.containsKey(child.getCity())) {
                 Integer count = cityWithCount.get(child.getCity());
@@ -26,6 +28,7 @@ public class SortByCity implements SortStrategy {
                 cityWithScores.put(child.getCity(), list);
             }
         }
+        // create a map with entries as city and average score of that city
         for (Map.Entry<Cities, Integer> entry : cityWithCount.entrySet()) {
             Integer count = entry.getValue();
             Cities city = entry.getKey();
@@ -37,25 +40,17 @@ public class SortByCity implements SortStrategy {
             Double cityAverage = (Double) (sum / count);
             cityWithAverage.put(city, cityAverage);
         }
-//        cityWithAverage = cityWithAverage.entrySet().stream().sorted((o1, o2) -> {
-//            if (Double.compare(o1.getValue(), o2.getValue()) == 0) {
-//                return o1.getKey().name().compareTo(o2.getKey().name());
-//            } else {
-//                return Double.compare(o1.getValue(), o2.getValue());
-//            }
-//        }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, Hashtable::new));
-//        TreeMap<Cities, Double> sorted = new TreeMap<>(cityWithAverage);
+        // create the sorted list of cities by the average score
         List<Cities> sorted = new ArrayList<>(cityWithAverage.keySet());
-        Collections.sort(sorted, new Comparator<Cities>() {
-            @Override
-            public int compare(Cities o1, Cities o2) {
-                if (Double.compare(cityWithAverage.get(o1), cityWithAverage.get(o2)) == 0) {
-                    return o1.name().compareTo(o2.name());
-                } else {
-                    return Double.compare(cityWithAverage.get(o2), cityWithAverage.get(o1));
-                }
+        Collections.sort(sorted, (o1, o2) -> {
+            if (Double.compare(cityWithAverage.get(o1), cityWithAverage.get(o2)) == 0) {
+                return o1.name().compareTo(o2.name());
+            } else {
+                return Double.compare(cityWithAverage.get(o2), cityWithAverage.get(o1));
             }
         });
+        // sort the children by the city list, and ascending by the id
+        // they are already sorted by the id so just traverse the list
         List<Child> sortedChildren = new ArrayList<>();
         for (Cities city : sorted) {
             for (Child child : children) {
